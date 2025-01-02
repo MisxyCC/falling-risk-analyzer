34a773fe1f6d4471baba73f39bc987f76347f8f0
513747 <supakorn.chu@pea.co.th>
2025-01-02 15:58:51 +0700

    Removed the footer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
 import NavigationBar from '@/components/layout/NavigationBar.vue';
-import Footer from '@/components/layout/Footer-component.vue';
 </script>
 
 <template>
   <NavigationBar></NavigationBar>
   <RouterView />
-  <Footer></Footer>
 </template>
